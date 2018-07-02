7b013dafe320bb9934cca5316afa6a3655ab959d
Killykan <killyk4n@gmail.com>
2018-07-03 00:16:49 +0200

    Responsive phase 1 : css changes for mobil devices.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoia2lsbHlrYW4iLCJhIjoiY2pqM2R3NzRvMHE4NzNrbXFwbTRra3c0YiJ9.pcqt8HV3NdPE0fG3kUL5NQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
